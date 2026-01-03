2dbb3959fbf50dcf6db728b8f2454d724766e38d
Ármis <nino_lindo10@outlook.com>
2026-01-02 23:35:57 -0300

    Operadores

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            // tipos de variaveis
             int idade = 25;           // numero inteiro
             long numero_palnetas = 8; // numero inteiro grande
             short numero_estados = 24;// numero inteiro pequeno
@@ -14,11 +15,23 @@
             ulong numero2 = 8;        // somente numeros iteiros grande positivos
             ushort numero3 = 24;      // somente numeros iteiros pequeno positivos
 
-            double altura = 1.75f;    // numero decimal
+            float dinheiro = 1.99f;   // numero decimal simples 7 digitos
+            double altura = 1.75f;    // numero decimal 16 digitos
+            decimal imposto = 3.77M;  // numero decimal 29 digitos
+
             string nome = "Nino";     // texto
+            char letra = 'A';         // caractere
+
             bool ligado = true;       // verdadeiro(true) ou falso(false)
 
+            object livro = null;      // tipo base de todos os outros tipos em c#
+            dynamic capa = null;      // tipo dinâmico, definido em tempo de execução
             var pais = "Brasil";      // o compilador entende que é string
+
+            //operadores
+            nome = idade >= 18 ? "É nino" : "Não é nino";
+            pais = capa ?? "não tem livro";
+
 
             Console.WriteLine("Hello, World!");
 
